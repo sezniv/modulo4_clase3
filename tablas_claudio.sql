7bbf43dbe099be1df8be72636e733999d6a45984
claudio.caniullan <claudio.caniullan@usach.cl>
2020-12-06 20:44:34 -0300

    delete mamifero_insecto, se agregan archivos insert y tablas

---
+++ tablas_claudio.sql
@@ -0,0 +1,15 @@
+CREATE TABLE encargados (id_encargados INT,
+			 supervisor_encargados INT,
+			 nombre_encargados VARCHAR(45),
+			 numero_encargado INT,
+			 id_area INT,
+			 id_zoo INT);
+
+
+
+
+CREATE TABLE animal_x_encargados (id_animal VARCHAR(30),
+				  id_categoria VARCHAR(10),
+				  id_encargados INT,
+				  id_area INT,
+				  id_zoo INT);
